2ccccb514425a3868e3510a674aaf8bdfed2b0bc
Ebnen <Ebenzy1996@gmail.com>
2024-10-03 20:03:31 +0100

    interview

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""implementing Pascal’s Triangle in Python"""
+
+
+def pascal_triangle(n):
+    """Function that returns a list of lists of integers
+       representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    pas_tri = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                prev_row = pas_tri[i - 1]
+                prev_val_l = prev_row[j - 1] if j - 1 < len(prev_row) else 0
+                prev_val_r = prev_row[j] if j < len(prev_row) else 0
+                row.append(prev_val_l + prev_val_r)
+        pas_tri.append(row)
+    return pas_tri
