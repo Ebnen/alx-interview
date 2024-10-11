20dbbb30b589d580dc2684136ac975f9f00be4ba
Ebnen <Ebenzy1996@gmail.com>
2024-10-11 17:08:02 +0100

    gaay

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,14 +4,14 @@
 
 def canUnlockAll(boxes):
     """interview question assignment"""
-    n = len(boxes) 
-    unlocked = [False] * n 
+    n = len(boxes)
+    unlocked = [False] * n
     unlocked[0] = True
-    keys = [0] 
+    keys = [0]
     while keys:
-        key = keys.pop() 
+        key = keys.pop()
         for new_key in boxes[key]:
             if new_key < n and not unlocked[new_key]:
-                unlocked[new_key] = True 
-                keys.append(new_key) 
-    return all(unlocked) 
+                unlocked[new_key] = True
+                keys.append(new_key)
+                return all(unlocked)
