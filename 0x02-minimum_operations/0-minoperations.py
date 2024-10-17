47973265736e938a7c21255e665dba70dd52a5b2
Ebnen <Ebenzy1996@gmail.com>
2024-10-17 19:29:13 +0100

    interview

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""operations to make n equal to 1"""
+
+
+def minOperations(n):
+    """minOperations function"""
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+    
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    
+    return operations
